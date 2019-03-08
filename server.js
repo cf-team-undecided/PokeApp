7d033165a63e7b365e1088670d7bf351445f1815
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-07 17:56:01 -0800

    Random 'mon feature complete

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // App Dependencies
 const express = require('express');
 const superagent = require('superagent');
-// const methodOverride = require('method-override');
 const pg = require('pg');
 
 // Load environment variables from .env file
@@ -27,19 +26,15 @@ app.use(express.static('./public'));
 // Set the view engine for templating
 app.set('view engine', 'ejs');
 
-// Middleware to handle DELETE
-// app.use(methodOverride((request, response) => {
-//   if (request.body && typeof request.body === 'object' && '_method' in request.body) {
-//     // look in urlencoded POST bodies and delete it
-//     let method = request.body._method;
-//     delete request.body._method;
-//     return method;
-//   }
-// }))
-
 // Routes
 
-app.get('/', (request, response) => response.render('./index'));
+app.get('/', (request, response) => {
+  getRandomPokemon()
+    .then( (randomMon) => {
+      console.log('rando', randomMon)
+      response.render('./index', {pokemon: randomMon})
+    })
+});
 
 app.get('/search', showSearch);
 
@@ -87,6 +82,7 @@ function PokemonDetails(pokemon) {
   this.description = '';
   this.moves = [];
   this.favoritesArr = [];
+  this.randomMon = [];
 
 }
 
@@ -98,60 +94,66 @@ function displayDetails(request, response) {
   let SQL = `SELECT * FROM species WHERE national_dex_id=$1`
   let value = [request.params.id];
 
-  return client.query(SQL, value)
-    .then( (results) => {
-      let details = new PokemonDetails(results.rows[0])
+  return getRandomPokemon()
+  .then( (randomMon) => {
 
-      client.query(`SELECT * FROM favorites;`)
-        .then((favorites) => {
-          favorites.rows.forEach( (faves) => details.favoritesArr.push(faves.id));
+    return client.query(SQL, value)
+      .then( (results) => {
+        let details = new PokemonDetails(results.rows[0])
 
-          getDamageMods(details.typeOne, details.typeTwo)
-            .then( (modResults) => {
-              modResults.forEach( (val, idx) => {
-                if( val > 1 ) {
-                  details.weak.push(getTypeName(idx));
-                }
-                if( val < 1 ) {
-                  details.strong.push(getTypeName(idx));
-                }
-              })
+        client.query(`SELECT * FROM favorites;`)
+          .then((favorites) => {
+            favorites.rows.forEach( (faves) => details.favoritesArr.push(faves.id));
 
-              getFlavorText(details.id)
-                .then( (flavorResults) => {
-                  let url = `https://pokeapi.co/api/v2/pokemon/${details.id}`;
-                  details.description = flavorResults.split('\n').join(' ')
+            getDamageMods(details.typeOne, details.typeTwo)
+              .then( (modResults) => {
+                modResults.forEach( (val, idx) => {
+                  if( val > 1 ) {
+                    details.weak.push(getTypeName(idx));
+                  }
+                  if( val < 1 ) {
+                    details.strong.push(getTypeName(idx));
+                  }
+                })
 
-                  superagent.get(url)
-                    .then(apiResponse => {
-                      apiResponse.body.moves.forEach( (move) => {
-                        let moveArr = [];
-                        if(move.version_group_details[0].level_learned_at >= 1) {
-                          moveArr.push(move.version_group_details[0].level_learned_at);
-                          moveArr.push(move.move.name);
-                          details.moves.push(moveArr);
-                        }
+                getFlavorText(details.id)
+                  .then( (flavorResults) => {
+                    let url = `https://pokeapi.co/api/v2/pokemon/${details.id}`;
+                    details.description = flavorResults.split('\n').join(' ')
+
+                    superagent.get(url)
+                      .then(apiResponse => {
+                        apiResponse.body.moves.forEach( (move) => {
+                          let moveArr = [];
+                          if(move.version_group_details[0].level_learned_at >= 1) {
+                            moveArr.push(move.version_group_details[0].level_learned_at);
+                            moveArr.push(move.move.name);
+                            details.moves.push(moveArr);
+                          }
+                        })
+                        details.moves.sort( (a, b) => a[0] - b[0])
+                        response.render(`pages/detail`, {results: details, pokemon: randomMon} )
                       })
-                      details.moves.sort( (a, b) => a[0] - b[0])
-                      response.render(`pages/detail`, {pokemon: details} )
                     })
                     .catch(err => handleError(err, response))
-                })
-                .catch(err => handleError(err, response))
-            })
-            .catch(err => handleError(err, response))
-        })
-        .catch(err => handleError(err, response))
-    })
+                  })
+                  .catch(err => handleError(err, response))
+              })
+              .catch(err => handleError(err, response))
+          })
+          .catch(err => handleError(err, response))
+      })
     .catch(err => handleError(err, response))
 }
 
 function getRandomPokemon() {
-  let SQL = `SELECT * FROM species WHERE id=$1;`;
+  let SQL = `SELECT * FROM species WHERE national_dex_id=$1;`;
   let randomPokemon = Math.ceil(Math.random() * Math.ceil(807));
   let value = [randomPokemon];
-
-  return client.query(SQL, value);
+  return client.query(SQL, value)
+    .then( (results) => {
+      return results.rows[0];
+    })
 }
 
 function buildPokemonDatabase(id) {
@@ -219,10 +221,14 @@ function showSearch(request, response) {
   if (request.body.pages === undefined){SQL += 'LIMIT 20'}
   if (request.body.pages){ SQL += `ORDER BY national_dex_id OFFSET ${parseInt(request.body.pages)* 20} FETCH NEXT 20 ROWS ONLY`}
 
-  return client.query(SQL)
-    .then(result => {
-      response.render('./pages/search', {result: result.rows, types: ['none', 'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy']}
-      )
+  return getRandomPokemon()
+    .then( (randomMon) => {
+
+      return client.query(SQL)
+        .then(result => {
+          console.log('normal', result.rows[0])
+          response.render('./pages/search', {result: result.rows, types: ['none', 'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy'], pokemon: randomMon})
+        })
     })
     .catch(err => handleError(err, response))
 }
@@ -231,10 +237,15 @@ function searchBy(request, response) {
   let SQL = 'SELECT * FROM species WHERE ';
   if(request.body.search) {SQL += `name='${request.body.search}'`}
   if(request.body.search === '') {SQL += `type_primary_id='${parseInt(request.body.types)}'`}
-  return client.query(SQL)
-    .then(result => {
-      response.render('./pages/search', {result: result.rows, types: ['none', 'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy']}
-      )
+
+  return getRandomPokemon()
+    .then( (randomMon) => {
+
+      return client.query(SQL)
+        .then(result => {
+          response.render('./pages/search', {result: result.rows, types: ['none', 'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy'], pokemon: randomMon}
+          )
+        })
     })
     .catch(err => handleError(err, response))
 }
@@ -243,26 +254,29 @@ function showFavorites(request, response) {
   let SQL = 'SELECT * FROM species ';
   let fullArr = [];
 
-  return client.query(SQL)
-    .then (allPokemon => {
-      fullArr = (allPokemon.rows);
-      client.query(`SELECT * FROM favorites;`)
-        .then((favorites) => {
-          let favoritesArr = [];
-          let values = favorites.rows.map( faves => faves.id).sort( (a, b) => a - b);
+  return getRandomPokemon()
+    .then( (randomMon) => {
 
-          fullArr.forEach( (val) => {
-            values.forEach( (faveVal) => {
-              if (val.national_dex_id === faveVal) {
-                favoritesArr.push(val)
-              }
+      return client.query(SQL)
+        .then (allPokemon => {
+          fullArr = (allPokemon.rows);
+          client.query(`SELECT * FROM favorites;`)
+            .then((favorites) => {
+              let favoritesArr = [];
+              let values = favorites.rows.map( faves => faves.id).sort( (a, b) => a - b);
+
+              fullArr.forEach( (val) => {
+                values.forEach( (faveVal) => {
+                  if (val.national_dex_id === faveVal) {
+                    favoritesArr.push(val)
+                  }
+                })
+              })
+
+              response.render('./pages/favorites', {results: favoritesArr, types: ['none', 'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy'], pokemon: randomMon}
+              )
             })
-          })
-
-          response.render('./pages/favorites', {result: favoritesArr, types: ['none', 'normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy']}
-          )
         })
-        .catch(err => handleError(err, response))
     })
     .catch(err => handleError(err, response))
 }
